da7cd9c9a64c0ae861ec3f70e6919be1acf1660b
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2026-07-23 21:33:34 +0700

    fix deploy

--- app/(dashboard)/master/bahan-baku/page.tsx
+++ app/(dashboard)/master/bahan-baku/page.tsx
@@ -1,8 +1,16 @@
+import { Suspense } from "react";
 import { BahanBakuClient } from "./bahan-client";
 import { getBahanBaku } from "./actions";
 
-export default async function BahanBakuPage() {
+async function BahanBakuData() {
   const bahanBakuData = await getBahanBaku();
-  
   return <BahanBakuClient initialData={bahanBakuData} />;
+}
+
+export default function BahanBakuPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <BahanBakuData />
+    </Suspense>
+  );
 }
